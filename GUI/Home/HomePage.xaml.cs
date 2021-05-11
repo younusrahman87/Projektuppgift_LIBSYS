9455784c9692e4fc5c034d93bac74b57f67e2806
younusrahman <m.younusrahman@gmail.com>
2021-05-11 10:04:00 +0200

    New design

--- GUI/Home/HomePage.xaml.cs
+++ GUI/Home/HomePage.xaml.cs
@@ -22,102 +22,84 @@ namespace GUI.Home
     /// </summary>
     public partial class HomePage : Page
     {
+        string search_text = "Skriv title, författare eller annat sökord";
+        string Bt_name_bok = "Böcker";
+        string Bt_name_user = "Användare";
 
 
 
-        private string searchboxtxt = "-- SMART SÖK --";
-        private static object[] Current_user;
-        public static object[] _GCU { get { if (Current_user == null) { Current_user = new object[4]; } return Current_user;} }
+
         public HomePage()
         {
 
 
             InitializeComponent();
+            searchbox.Text = search_text;
 
-            
         }
 
-        private void Sing_out_Click(object sender, RoutedEventArgs e)
+        private void MenuBT_Click(object sender, RoutedEventArgs e)
+        {
+
+            Button Converted_to_Bt = sender as Button;
+
+            if(Converted_to_Bt.Content.Equals(Bt_name_bok))
+            {
+                if (Bookmenu.Visibility == Visibility.Visible) { Bookmenu.Visibility = Visibility.Collapsed; return; }
+                Bookmenu.Visibility = Visibility.Visible;
+                return;
+            }
+
+            else if (Converted_to_Bt.Content.Equals(Bt_name_user))
+            {
+                if (Usermenu.Visibility == Visibility.Visible) { Usermenu.Visibility = Visibility.Collapsed; return; }
+                Usermenu.Visibility = Visibility.Visible;
+                return;
+            }
+
+        }
+
+        private void SearchBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (searchbox.Text == search_text) searchbox.Text = string.Empty; return;
+
+            searchbox.Text = search_text;
+        }
+
+        private void searchBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (searchbox.Text == string.Empty) searchbox.Text = search_text; return;          
+
+
+        }
+
+
+
+        private void ExitUser_Click(object sender, RoutedEventArgs e)
         {
             NavigationService.Navigate(new LoginPage());
         }
 
-
-        private void Search_box_got_focuse(object sender, RoutedEventArgs e)
-        {
-            if (Search_box.Text.Equals(searchboxtxt))
-            { Search_box.Text = string.Empty; }
-        }
-
-
-
-
-        private void Search_box_lost_focuse(object sender, RoutedEventArgs e)
-        {
-            if (Search_box.Text.Equals(string.Empty))
-            { Search_box.Text = searchboxtxt; }
-        }
-
-
-
-
-
-        private void Search_BT_Click(object sender, RoutedEventArgs e)
-        {
-
-
-            SearchWindow search_obj = new SearchWindow();
-
-            search_obj.Show();
-            var mainwin = Application.Current.MainWindow;
-            mainwin.Hide();
-            Search_box.Text = searchboxtxt;
-
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
-        private void Profil_bt_Click(object sender, RoutedEventArgs e)
-        {
-
-            Profil_bt.BorderBrush = new SolidColorBrush(Colors.Green);
-            AddItemBT.BorderBrush = default;
-            AddUser_Admin.BorderBrush = default;
-
-            Menubar_frame.Navigate(new Profile());
-
-            //if (_GCU[0].Equals(Enum.GetName(typeof(IUserDataAccess.TypeOfUser), 1)))
-            //{ Menubar_frame.Navigate(new AdminProfile()); }
-            //else { Menubar_frame.Navigate(new Profile()); }
-
-        }
-
         private void AddItemBT_Click(object sender, RoutedEventArgs e)
         {
-            Profil_bt.BorderBrush= default;
-            AddUser_Admin.BorderBrush = default;
-            AddItemBT.BorderBrush = new SolidColorBrush(Colors.Green);
-           
+            Menubar_frame.Navigate(new AddItem());
 
-            Menubar_frame.Navigate(new AddItem() );
         }
 
-        private void add_User_Click(object sender, RoutedEventArgs e)
+
+        private void ChangeitemBT_Click(object sender, RoutedEventArgs e)
         {
-            Profil_bt.BorderBrush = default;
-            AddItemBT.BorderBrush = default;
-            AddUser_Admin.BorderBrush = new SolidColorBrush(Colors.Green);
+            Menubar_frame.Navigate(new ChangeRemoveBooks());
+        }
+
+        private void AdduserBT_Click(object sender, RoutedEventArgs e)
+        {
             Menubar_frame.Navigate(new AddUser());
+        }
+
+        private void ChangeuserBT_Click(object sender, RoutedEventArgs e)
+        {
+            Menubar_frame.Navigate(new ChangeRemoveUser());
         }
     }
 }
--- GUI/Login/LoginPage.xaml.cs
+++ GUI/Login/LoginPage.xaml.cs
@@ -61,6 +61,8 @@ namespace GUI.Login
             
 
         }
+
+
     }
 }
     
