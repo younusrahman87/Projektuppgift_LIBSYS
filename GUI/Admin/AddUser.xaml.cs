8a3c19f46853376d13ad1d584bdf14e18bdce89e
pontusbjornf <70628477+pontusbjornf@users.noreply.github.com>
2021-05-22 12:12:03 +0200

    UserPage Update
    Update userpage

--- GUI/Admin/AddUser.xaml.cs
+++ GUI/Admin/AddUser.xaml.cs
@@ -52,33 +52,6 @@ namespace GUI.Admin
 
         //}
 
-        private void cb_choose_user_typ_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            var CBox = sender as ComboBox;
-
-            if (CBox.SelectedIndex.Equals(0))
-            {
-                Sp_cb_card_have.Visibility = Visibility.Visible;
-                sp_Jobtitle.Visibility = Visibility.Collapsed;
-                SP_Button.Visibility = Visibility.Visible;
-                return;
-            }
-            else if (CBox.SelectedIndex.Equals(1) || CBox.SelectedIndex.Equals(2))
-            {
-                Sp_cb_card_have.Visibility = Visibility.Collapsed;
-                sp_Jobtitle.Visibility = Visibility.Visible;
-                SP_Button.Visibility = Visibility.Visible;
-                return;
-            }
-
-            Sp_cb_card_have.Visibility = Visibility.Collapsed;
-            sp_Jobtitle.Visibility = Visibility.Collapsed;
-            SP_Button.Visibility = Visibility.Collapsed;
-
-
-        }
-
-
 
 
         private void Got_focuse(object sender, RoutedEventArgs e)
@@ -120,8 +93,6 @@ namespace GUI.Admin
 
                 if (validation.AvailableEmail(tb_email.Text))
                 {
-                    if (cb_choose_user_typ.SelectedIndex.Equals(0))
-                    {
                         user.FirstName = tb_firstname.Text;
                         user.LastName = tb_lastname.Text;
                         user.Email = tb_email.Text;
@@ -129,28 +100,6 @@ namespace GUI.Admin
                         else { user.LibraryCard = false; }
                         user.Password = tb_password.Password;
                         service.AddUser(user);
-
-                    }
-                    else if (cb_choose_user_typ.SelectedIndex.Equals(1))
-                    {
-                        personal.FirstName = tb_firstname.Text;
-                        personal.LastName = tb_lastname.Text;
-                        personal.Email = tb_email.Text;
-                        personal.JobTitle = tb_Jobtitle.Text;
-                        personal.Password = tb_password.Password;
-                        service.AddPersonal(personal);
-
-                    }
-                    else if (cb_choose_user_typ.SelectedIndex.Equals(2))
-                    {
-                        personal.FirstName = tb_firstname.Text;
-                        personal.LastName = tb_lastname.Text;
-                        personal.Email = tb_email.Text;
-                        personal.JobTitle = tb_Jobtitle.Text;
-                        personal.Password = tb_password.Password;
-                        service.AddPersonal(personal);
-
-                    }
 
                     MessageBox.Show("Användare är tillagd", "Användare", MessageBoxButton.OK);
                 }
@@ -164,117 +113,6 @@ namespace GUI.Admin
 
 
         }
-
-        private void Bt_remove_case_Click(object sender, RoutedEventArgs e)
-        {
-
-            try
-            {
-                if (validation.RemoveEmailUser(tb_sc_email.Text))
-                {
-                    service.RemoveUser(tb_sc_email.Text);
-
-                    MessageBox.Show("Användare är nu borttagen", "Användare", MessageBoxButton.OK);
-                }
-                else if (validation.RemoveEmailPersonal(tb_sc_email.Text))
-                {
-                    service.RemovePersonal(tb_sc_email.Text);
-                    MessageBox.Show("Peronal är nu borttagen", "Användare", MessageBoxButton.OK);
-                }
-                else { MessageBox.Show("Felaktig inmating av email", "Användare", MessageBoxButton.OK); }
-               
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK);
-            }
-
-
-
-
-        }
-
-        private void search_bt_Click(object sender, RoutedEventArgs e)
-        {
-            //--------------------------------------------------------------Sök med hjälp av email
-
-           
-
-            try
-            {
-                if (validation.RemoveEmailUser(tb_sc_search_user.Text))
-                {
-                    cb_sc_choose_user_typ.SelectedIndex = 0;
-                    IEnumerable<UserDb> user = service.GetUserInfo(tb_sc_search_user.Text);
-
-                    foreach (var item in user)
-                    {
-                    tb_sc_firstname.Text = item.FirstName;
-                    tb_sc_lastname.Text = item.LastName;
-                    tb_sc_email.Text = item.Email;
-                    tb_sc_password.Password = item.Password;
-                    }
-                }
-               else if (validation.RemoveEmailPersonal(tb_sc_search_user.Text))
-                {
-                    cb_sc_choose_user_typ.SelectedIndex = 1;
-                    IEnumerable<PersonalDb> personal = service.GetPersonalInfo(tb_sc_search_user.Text);
-
-                    foreach (var item in personal)
-                    {
-                        tb_sc_firstname.Text = item.FirstName;
-                        tb_sc_lastname.Text = item.LastName;
-                        tb_sc_email.Text = item.Email;
-                        tb_sc_password.Password = item.Password;
-                    }
-                }
-                else
-                { MessageBox.Show("Felaktig inmating av email", "Användare", MessageBoxButton.OK); }
-             
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK);
-            }
-
-        }
-
-        private void Bt_edit_mekanik_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                if (validation.RemoveEmailUser(tb_sc_email.Text))
-                {
-                    user.FirstName = tb_sc_firstname.Text;
-                    user.LastName = tb_sc_lastname.Text;
-
-                    user.Email = tb_sc_email.Text;
-                    user.Password = tb_sc_password.Password;
-                    service.UpdateUser(user);
-
-                    MessageBox.Show("Användare är nu uppdaterad", "Användare", MessageBoxButton.OK);
-                }
-                else if (validation.RemoveEmailPersonal(tb_sc_email.Text))
-                {
-                    personal.FirstName = tb_sc_firstname.Text;
-                    personal.LastName = tb_sc_lastname.Text;
-
-                    personal.Email = tb_sc_email.Text;
-                    personal.Password = tb_sc_password.Password;
-                    service.UpdatePersonal(personal);
-
-
-                    MessageBox.Show("Peronal är nu uppdaterad", "Användare", MessageBoxButton.OK);
-                }
-                else { MessageBox.Show("Felaktig inmating av email", "Användare", MessageBoxButton.OK); }
-
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK);
-            }
-
-
-        }
+       
     }
 }
--- GUI/Admin/ChangeRemoveUser.xaml.cs
+++ GUI/Admin/ChangeRemoveUser.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using GUI.Models;
+using Logic;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -18,10 +21,46 @@ namespace GUI.Admin
     /// </summary>
     public partial class ChangeRemoveUser : Page
     {
+        private FuncService service = new Service();
+        private IValidation validation = new Logic.Validation();
+        private UserDb user { get; set; } = new UserDb();
+ 
         public ChangeRemoveUser()
         {
             InitializeComponent();
+            GetListview();
+
+
+
+        }
+      
+        public void GetListview()
+        {
+            GetData getData = new GetData();
+            var users = getData.GetUser();
+            foreach (var item in users)
+            {
+                listView.Items.Add(item);
+
+            }
         }
 
+        private void listView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UserDb user = (UserDb)listView.SelectedItem;
+
+            firstName.Text = user.FirstName.ToString();
+           
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Ändra
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            //Ta bort
+        }
     }
 }
